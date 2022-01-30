34da4153b57767da6c346c0bcd140ec585999ffd
Anand Jha <anandjha7424@gmail.com>
2022-01-30 09:37:33 +0530

    nav and main coded.

---
+++ app.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   btn.classList.toggle("altcolor");
+// });
+
+// function toggle() {
+//   button.classList.toggle("altcolor");
+// }
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "red";
+  document.body.style.backgroundColor = "black";
+});
